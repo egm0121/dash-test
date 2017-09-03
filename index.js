2eb1bef69fb3ec8040e38848bdd8cc5ea44398e1
Giulio Dellorbo <egm0121@gmail.com>
2017-09-03 13:19:44 +0100

    fire a get to blue-light server to toggle light

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const DashButton = require('dash-button');
+const Http = require('http');
 
 const DASH_BUTTON_MAC_ADDRESS = require('./config.json').mac;
 
@@ -6,5 +7,6 @@ let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
 button.addListener(() => {
   console.log('hey amazon dash button pressed 💪🏼');
+  Http.get('http://localhost:2222/light/dim/toggle',() => {});
 });
 
